81d4d0e7511fdd9d2446affc99b08e94cf6375a1
Zach_Cook_Lptp <zachcook8@gmail.com>
2021-04-15 11:28:36 -0500

    Added all of the images to the images folder for each pair. Created a file that will import all of the images and create an object. This will then be imported into the usdpairs js file to then be displayed

---
+++ frontend/src/images/imageimport.js
@@ -0,0 +1,82 @@
+import AAVE from './AAVE.svg'
+import ADA from './ADA.svg'
+import AMP from './AMP.svg'
+import AMPL from './AMPL.svg'
+import ADX from './ADX.png'
+import BAND from './BAND.svg'
+import BCH from './BCH.svg'
+import BNB from './BNB.svg'
+import BNT from './BNT.svg'
+import BTC from './BTC.svg'
+import COMP from './COMP.svg'
+import COVER from './COVER.svg'
+import DASH from './DASH.svg'
+import DOT from './DOT.svg'
+import EOS from './EOS.svg'
+import ETC from './ETC.svg'
+import ETH from './ETH.svg'
+import FIL from './FIL.svg'
+import FNX from './FNX.svg'
+import HEGIC from './HEGIC.svg'
+import INJ from './INJ.svg'
+import IOST from './IOST.png'
+import KNC from './KNC.svg'
+import LINK from './LINK.svg'
+import LTC from './LTC.svg'
+import MATIC from './MATIC.svg'
+import OXT from './OXT.svg'
+import REN from './REN.svg'
+import SNX from './SNX.svg'
+import SXP from './SXP.svg'
+import TRU from './TRU.svg'
+import TRX from './TRX.svg'
+import UNI from './UNI.svg'
+import WAVES from './WAVES.svg'
+import XHV from './XHV.svg'
+import XMR from './XMR.svg'
+import XRP from './XRP.svg'
+import XTZ from './XTZ.svg'
+import YFI from './YFI.svg'
+
+
+export const images = {
+    AAVE: AAVE,
+    ADA: ADA,
+    AMP: AMP,
+    AMPL: AMPL,
+    ADX: ADX,
+    BAND: BAND, 
+    BCH: BCH, 
+    BNB:BNB, 
+    BNT:BNT, 
+    BTC:BTC, 
+    COMP: COMP, 
+    COVER: COVER, 
+    DASH: DASH, 
+    DOT: DOT, 
+    EOS: EOS, 
+    ETC: ETC, 
+    ETH: ETH, 
+    FIL: FIL, 
+    FNX: FNX, 
+    HEGIC: HEGIC, 
+    INJ: INJ, 
+    IOST: IOST, 
+    KNC: KNC, 
+    LINK: LINK, 
+    LTC: LTC, 
+    MATIC: MATIC, 
+    OXT: OXT, 
+    REN: REN, 
+    SNX: SNX, 
+    SXP: SXP, 
+    TRU: TRU,  
+    TRX: TRX, 
+    UNI: UNI, 
+    WAVES: WAVES, 
+    XHV: XHV, 
+    XMR: XMR, 
+    XRP: XRP, 
+    XTZ: XTZ, 
+    YFI: YFI 
+}
