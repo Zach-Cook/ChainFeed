595bc53463340bec7faa2e464c229ecc722c766e
Zach_Cook_Lptp <zachcook8@gmail.com>
2021-04-16 17:00:13 -0500

    Added some revisions to the About text for better clarity and reabability

--- frontend/src/containers/about.js
+++ frontend/src/containers/about.js
@@ -18,14 +18,14 @@ export default function AboutContainer(){
                 <About.Horizontal/>
                 <About.ContentFrame>
                     <About.ContentTitle>---- The Oracle Problem</About.ContentTitle>
-                    <About.ParagraphText>Blockchains are great at managing the state of on-chain data but have no inherent access to important information off-chain. For the success of Decentralized Blockchain technology, Blockchain networks must have access to important information that lies off-chain. For example, how would a smart contract have access to the price of wheat? How does this data get on-chain while maintaining decentralization?</About.ParagraphText>
+                    <About.ParagraphText>Blockchains are great at managing the state of on-chain data but have no built in access to important information off-chain. For the success of Decentralized Blockchains, these networks must have access to important information that situate off-chain. For example, how would a smart contract have access to the price of wheat? How does this data get on-chain while maintaining decentralization?</About.ParagraphText>
                     <About.ContentTitle>---- The Solution</About.ContentTitle>
-                    <About.ParagraphText>To get this off-chain data onto a Blockchain, we use an oracle. An oracle is a piece of software that inputs off-chain data onto the chain. Although this provides access to external data, we must ensure that this data is verifiable and not falsely manipulated by one single party. Chainlink provides this solution.</About.ParagraphText>
+                    <About.ParagraphText>To get this off-chain data onto a Blockchain, we use an oracle. An oracle is software that inputs off-chain data onto the chain. Although this solves the problem of getting outside data onto the chain, a new problem arises. How does a blockchain verify that this information can be trusted? Trusting a single party to provide off-chain knowledge re-introduces the risk of centralization.</About.ParagraphText>
                     <About.ContentTitle>---- Chainlink</About.ContentTitle>
-                    <About.ParagraphText>Chainlink is a network of oracles that provide verifiable external data to a single point that is callable by a smart contract. Per source, there is a network of parties called oracles, that retrieve, validate, and deliver data to one single source of truth. Now, smart contracts can have access to trustworthy information.</About.ParagraphText>
+                    <About.ParagraphText>Chainlink is a network of oracles that provide verifiable external data to a blockchain. Per source of data, there are numbers of oracles, that retrieve, validate, and deliver data on-chain. The implementation of Chainlink allows smart contracts to trustlessly interact with off chain data. This solves the inherent oracle problem while maintaining the integrity of decentralization.</About.ParagraphText>
                     <About.ContentTitle>---- Learn More</About.ContentTitle>
                     <About.LearnMoreFrame>
-                        <About.ParagraphText>For a more information please visit the Chainlink Blog: </About.ParagraphText>
+                        <About.ParagraphText>For more information visit the Chainlink Blog: </About.ParagraphText>
                         <About.ParagraphText
                             textDecoration='underline'
                             marginLeft='10px'
